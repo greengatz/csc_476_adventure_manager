cb99328ba6260b2d9bc07f6fb0c4dc367f108ae1
bclarksoftware <bclarksoftware@gmail.com>
2015-04-20 15:07:49 -0700

    Added wagon files to begin working on movement on terrain.(after 25%, I will convert most objects into our more general object maker that daniel made.

---
+++ Lab1Template/source/wagon.h
@@ -0,0 +1,36 @@
+/**
+ * Wagon.h - Header file for Wagon object.
+ * @author Brandon Clark
+ */
+
+#pragma once
+#ifndef _WAGON_H_
+#define _WAGON_H_
+
+#include "GLSL.h"
+#include "tiny_obj_loader.h"
+ #include "TextureLoader.h"
+
+class Wagon
+{
+public:
+	Wagon();
+	virtual ~Wagon();
+	void init(TextureLoader* texLoader);
+	void draw(GLint h_pos, GLint h_nor, GLint h_aTexCoord);
+	void setPosition(float x, float y, float z);
+	void setScale(float aScale);
+	void setRotation(float aRotation);
+	
+private:
+	std::vector<tinyobj::shape_t> shapes;
+	GLuint posBufID;
+	GLuint norBufID;
+	GLuint texBufID;
+	GLuint indBufID;
+	glm::vec3 position;
+	float scale;
+	float rotate;
+};
+
+#endif
