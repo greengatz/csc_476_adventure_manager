52429e8ca1cd11849b17a9c1082987028f695ab2
Mitch <mitchlane44@gmail.com>
2015-05-09 17:21:51 -0700

    main clean

--- Lab1Template/source/main.cpp
+++ Lab1Template/source/main.cpp
@@ -268,10 +268,10 @@ bool installShaders(const string &vShaderName, const string &fShaderName)
 	return true;
 }
 
-void test()
-{
-	cout << "test funct pointer" << endl;
-}
+// void test()
+// {
+// 	cout << "test funct pointer" << endl;
+// }
 
 void drawGL()
 {
@@ -709,12 +709,12 @@ int main(int argc, char **argv)
   	dtDraw = 0;
   	audio.playBackgroundMusic(true);
 
-  	vector<string> about;
-	about.push_back("about test");
-	option testOpt = {"test option", test};
-	vector<option> options;
-	options.push_back(testOpt);
-	menu.setData("Title", about, options);
+ //  	vector<string> about;
+	// about.push_back("about test");
+	// option testOpt = {"test option", test};
+	// vector<option> options;
+	// options.push_back(testOpt);
+	// menu.setData("Title", about, options);
    do{
    	timeNew = glfwGetTime();
 		audio.checkTime();
